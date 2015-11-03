5100d19dc69b705755a49ce385ffc8e085c00b89
Sil3nz3r <tuvu23061992@gmail.com>
2015-11-03 14:44:17 -0600

    new stuffs

--- Project2_DiskFiles/Program.cpp
+++ Project2_DiskFiles/Program.cpp
@@ -5,83 +5,87 @@
 */
 
 #include <windows.h>
-#include <wchar.h>
 #include <stdio.h>
-#include <locale.h>
-#include <stdlib.h>
+#include <string.h>
 #include <string>
 
-void record();
-void playback();
+using std::string;
+
+void record(const string &);
+void playback(const string &);
+
+const size_t max_capacity = MAX_PATH;
 
 void main()
 {
-    char buffer[MAX_PATH];
-    wchar_t wtext[MAX_PATH];
-    size_t size = 260;
-    //mbstowcs_s(size, wtext, buffer, strlen(buffer) + 1, size);
-
-    GetModuleFileName(NULL, wtext, MAX_PATH);
-
-    //size_t pos = 
-
-    record();
-    //playback();
-}
-
-void record()
-{
+    // Declare necessary variables
     DWORD count;
-    char data[250] = { 0 };
-    char prompt[] = "Enter some text. When you are done, enter quit.\n";
+    char data[max_capacity] = { 0 };
+    char prompt[] = ">>> How many files do you want to generate?\n";
     HANDLE file, input, output;
 
-    /*
-        Notes: The actual drive location where this lab writes the file
-        may be dependent upon your computer access privileges so you may
-        have to change the drive letter to whatever you desire
-    */
-    TCHAR fileName[] = TEXT("D:\\record.out");
-
+    // Initialize I/O handles
     input = GetStdHandle(STD_INPUT_HANDLE);
     output = GetStdHandle(STD_OUTPUT_HANDLE);
-    file = CreateFile(fileName, GENERIC_WRITE, 0, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
 
+    // Prompt the user and read file
     WriteFile(output, prompt, sizeof(prompt) - 1, &count, NULL);
-    ReadFile(input, data, 250, &count, NULL);
+    ReadFile(input, data, max_capacity, &count, NULL);
+
+    // Get the directory of the current process
+    char myCurrentDir[max_capacity];
+    GetCurrentDirectory(max_capacity, myCurrentDir);
+
+    // record and playback file
+    record(myCurrentDir);
+    playback(myCurrentDir);
+}
+
+void record(const string & currentDir)
+{
+    // Declare necessary variables
+    DWORD count;
+    char data[max_capacity] = { 0 };
+    char prompt[] = ">>> Enter some text. When you are done, enter quit.\n";
+    HANDLE file, input, output;
+
+    // Initialize handles and the necessary files
+    input = GetStdHandle(STD_INPUT_HANDLE);
+    output = GetStdHandle(STD_OUTPUT_HANDLE);
+    file = CreateFile((currentDir + "\\file1.txt").c_str(), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    // Prompt the user and read file
+    WriteFile(output, prompt, sizeof(prompt) - 1, &count, NULL);
+    ReadFile(input, data, max_capacity, &count, NULL);
 
     while (strncmp(data, "quit", 4) != 0)
     {
         WriteFile(file, data, count, &count, NULL);
-        ReadFile(input, data, 250, &count, NULL);
+        ReadFile(input, data, max_capacity, &count, NULL);
     }
     CloseHandle(file);
 }
 
-void playback()
+void playback(const string & currentDir)
 {
+    // Declare necessary variables
     DWORD count = -1;
-    char data[250] = { 0 };
-    char prompt[] = "Here is the content of your last recording session:\n";
+    char data[max_capacity] = { 0 };
+    char prompt[] = ">>> Here is the content of your last recording session:\n";
     HANDLE file, output;
+
+    // Initialize handles and the necessary files
     output = GetStdHandle(STD_OUTPUT_HANDLE);
+    file = CreateFile((currentDir + "\\file1.txt").c_str(), GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
-    /*
-        Note: the actual drive location where this lab writes the file
-        may be dependent upon your computer access priviledges so you may
-        have to change the drive letter to whatever you desire
-    */
-    TCHAR fileName[] = TEXT("D:\\record.out");
-
-    file = CreateFile(fileName, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-
+    // Prompt the user and read file
     WriteFile(output, prompt, sizeof(prompt) - 1, &count, NULL);
-    ReadFile(file, data, 250, &count, NULL);
+    ReadFile(file, data, max_capacity, &count, NULL);
 
     while (count != 0)
     {
         WriteFile(output, data, count, &count, NULL);
-        ReadFile(file, data, 250, &count, NULL);
+        ReadFile(file, data, max_capacity, &count, NULL);
     }
     CloseHandle(file);
     Sleep(5000);
