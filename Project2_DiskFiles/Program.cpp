7e3bdf79a0392a13f3fd321822e36920e3196bb9
Tu Vu <tuvu23061992@gmail.com>
2015-11-06 08:45:35 -0600

    Final checkin

--- Project2_DiskFiles/Program.cpp
+++ Project2_DiskFiles/Program.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <exception>
 
 using std::cin;
 using std::cout;
@@ -19,10 +20,11 @@ using std::string;
 using std::left;
 using std::endl;
 
-void showMenu();
+string showMenu();
+void showGoodbye();
 void record(const string &);
 void playback(const string &);
-void doStuff(const string &);
+void startProgram(const string &);
 bool isFileExisted(const string &);
 DWORD getCreationDisposition(const string &);
 
@@ -33,46 +35,48 @@ void main()
 {
     while (true)
     {
-        showMenu();
-
-        string input;
-        cin >> input;
+        string input = showMenu();
         if (input == "exit")
         {
-            cout << "Thanks for using the program. Goodbye!";
-            Sleep(7000);
+            showGoodbye();
             return;
         }
         else
         {
-            doStuff(input);
+            try
+            {
+                startProgram(input);
+            }
+            catch (const std::exception&)
+            {
+                showGoodbye();
+                return;
+            }
         }
-        //// Get the directory of the current process
-        //char myCurrentDir[max_capacity];
-        //GetCurrentDirectory(max_capacity, myCurrentDir);
-
-        //// record and playback file
-        //record(myCurrentDir);
-        //playback(myCurrentDir);
     }
 }
 
-void showMenu()
+string showMenu()
 {
     cout << setfill('-') << setw(40) << "-" << endl
         << setfill(' ') << setw(40) << left << "| Please choose an item to generate. Enter 'exit' to exit" << endl
         << setfill('-') << setw(40) << "-" << endl;
     cout << setfill(' ');
-    cout << setw(26) << left << "h for hamburger" << endl
-        << setw(26) << left << "p for pizza" << endl
-        << setw(26) << left << "s for soup" << endl;
+    cout << setw(26) << left << "+++ h for hamburger" << endl
+        << setw(26) << left << "+++ p for pizza" << endl
+        << setw(26) << left << "+++ s for soup" << endl;
+    string selection;
+    cin >> selection;
+    return selection;
 }
 
-void doStuff(const string & selectedOption)
+void startProgram(const string & selectedOption)
 {
+    // Get the current director of the project
     char myCurrentDir[max_capacity];
     GetCurrentDirectory(max_capacity, myCurrentDir);
 
+    // 
     if (selectedOption == "h") strcat_s(myCurrentDir, TEXT("\\hamburger.txt"));
     else if (selectedOption == "p") strcat_s(myCurrentDir, TEXT("\\pizza.txt"));
     else if (selectedOption == "s") strcat_s(myCurrentDir, TEXT("\\soup.txt"));
@@ -82,12 +86,39 @@ void doStuff(const string & selectedOption)
         return;
     }
     record(myCurrentDir);
+    playback(myCurrentDir);
 }
 
 inline bool isFileExisted(const string& name)
 {
     struct stat buffer;
     return (stat(name.c_str(), &buffer) == 0);
+}
+
+DWORD getCreationDisposition(const string & fileDir)
+{
+    if (isFileExisted(fileDir))
+    {
+        while (true)
+        {
+            string input;
+            cout << ">>> The file has already exists.\n";
+            playback(fileDir);
+            cout << ">>> What do you want to do? 'o' to overwrite or 'i' to insert.\n";
+            cin >> input;
+
+            if (input == "o") { return CREATE_ALWAYS; }
+            else if (input == "i") { return OPEN_EXISTING; }
+            else
+            {
+                cout << invalidInputMsg << endl;
+            }
+        }
+    }
+    else
+    {
+        return CREATE_NEW;
+    }
 }
 
 void record(const string & fileDir)
@@ -102,44 +133,27 @@ void record(const string & fileDir)
     input = GetStdHandle(STD_INPUT_HANDLE);
     output = GetStdHandle(STD_OUTPUT_HANDLE);
 
+    // Set the file creation disposition type
     DWORD creationDisposition = getCreationDisposition(fileDir);
 
+    // get a hold of the file we need
     file = CreateFile(fileDir.c_str(), GENERIC_WRITE, 0, NULL, creationDisposition, FILE_ATTRIBUTE_NORMAL, NULL);
 
     // Prompt the user and read file
     WriteFile(output, prompt, sizeof(prompt) - 1, &count, NULL);
     ReadFile(input, data, max_capacity, &count, NULL);
 
-    while (strncmp(data, "quit", 4) != 0)
+    while (strncmp(data, "done", 4) != 0)
     {
+        if (strncmp(data, "exit", 4) == 0)
+        {
+            throw std::exception();
+        }
+
         WriteFile(file, data, count, &count, NULL);
         ReadFile(input, data, max_capacity, &count, NULL);
     }
     CloseHandle(file);
-}
-
-DWORD getCreationDisposition(const string & fileDir)
-{
-    if (isFileExisted(fileDir))
-    {
-        while (true)
-        {
-            char input;
-            cout << "The file has already exists. Enter 'o' to overwrite or 'a' append\n";
-            input = cin.get();
-
-            if (input == 'o') { return CREATE_ALWAYS; }
-            else if (input == 'a') { return OPEN_EXISTING; }
-            else
-            {
-                cout << invalidInputMsg << endl;
-            }
-        }
-    }
-    else
-    {
-        return CREATE_NEW;
-    }
 }
 
 void playback(const string & fileDir)
@@ -147,12 +161,12 @@ void playback(const string & fileDir)
     // Declare necessary variables
     DWORD count = -1;
     char data[max_capacity] = { 0 };
-    char prompt[] = ">>> Here is the content of your last recording session:\n";
+    char prompt[] = ">>> Here is the current content of the file:\n";
     HANDLE file, output;
 
     // Initialize handles and the necessary files
     output = GetStdHandle(STD_OUTPUT_HANDLE);
-    file = CreateFile((fileDir + "\\file1.txt").c_str(), GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    file = CreateFile(fileDir.c_str(), GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
     // Prompt the user and read file
     WriteFile(output, prompt, sizeof(prompt) - 1, &count, NULL);
@@ -164,5 +178,10 @@ void playback(const string & fileDir)
         ReadFile(file, data, max_capacity, &count, NULL);
     }
     CloseHandle(file);
-    Sleep(5000);
+}
+
+void showGoodbye()
+{
+    cout << "Thanks for using the program. Goodbye!";
+    Sleep(7000);
 }
